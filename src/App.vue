cfd2a20bed6aa82d0ddcd9f15588f518b844c50d
Arty <colomborin53@gmail.com>
2022-11-25 20:48:25 +0200

    deleted excessive code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <center><h1>Hello Vue!</h1></center>
     </div>
     <router-view/>
   </div>
